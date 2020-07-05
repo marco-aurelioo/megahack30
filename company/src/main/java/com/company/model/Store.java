26cae2809911c0ff94040638dfc753ddf9f93bd8
TIOZAO <marco_aurelioo@yahoo.com.br>
2020-07-04 22:55:16 -0300

    add dados de endereco para lojas

--- company/src/main/java/com/company/model/Store.java
+++ company/src/main/java/com/company/model/Store.java
@@ -6,6 +6,33 @@ public class Store {
 
     private UUID id;
     private String name;
+    private String endereco;
+    private float latitude;
+    private float longitude;
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(float latitude) {
+        this.latitude = latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
 
     public UUID getId() {
         return id;
--- company/src/main/resources/data.sql
+++ company/src/main/resources/data.sql
@@ -12,18 +12,21 @@ CREATE TABLE STORE (
     id VARCHAR(250) NOT NULL,
     name VARCHAR(250) NOT NULL,
     company_id VARCHAR(250) NOT NULL,
-    document VARCHAR(250) NOT NULL
+    document VARCHAR(250) NOT NULL,
+    endereco VARCHAR(250) NULL,
+    latitude FLOAT  NULL,
+    longitude FLOAT NULL
 );
 
 INSERT INTO COMPANY (id, user_owner, name, document) VALUES
 ('dca57f7d-944b-49f0-aaa3-185abb5dc4bf','0635c81f-3e75-4e0f-a248-a919e9f0dde0','company fred 1','documento 1'),
 ('63809460-01d7-4fa8-af23-fd296aa5abd5','0635c81f-3e75-4e0f-a248-a919e9f0dde0','company fred 2','documento 2');
 
-INSERT INTO STORE (id, name, company_id, document) VALUES
-('12bccedb-e862-4181-b399-362bd4958073','A','dca57f7d-944b-49f0-aaa3-185abb5dc4bf','docA'),
-('680692d0-923d-4c08-907b-35b04e9b35d1','B','dca57f7d-944b-49f0-aaa3-185abb5dc4bf','docB'),
-('47971a91-b192-4198-b8c5-9a84cc8e2267','C','dca57f7d-944b-49f0-aaa3-185abb5dc4bf','docC'),
-('f37e3972-0c1f-43c8-8e61-53ec825f3060','D','63809460-01d7-4fa8-af23-fd296aa5abd5','docD');
+INSERT INTO STORE (id, name, company_id, document,endereco,latitude,longitude) VALUES
+('12bccedb-e862-4181-b399-362bd4958073','A','dca57f7d-944b-49f0-aaa3-185abb5dc4bf','docA','Av Paulista',-23.5585476,-46.664819),
+('680692d0-923d-4c08-907b-35b04e9b35d1','B','dca57f7d-944b-49f0-aaa3-185abb5dc4bf','docB','Rua Consolacao',-23.5513336,-46.6506514),
+('47971a91-b192-4198-b8c5-9a84cc8e2267','C','dca57f7d-944b-49f0-aaa3-185abb5dc4bf','docC','Av liberdade',-23.5513336,-46.6506514),
+('f37e3972-0c1f-43c8-8e61-53ec825f3060','D','63809460-01d7-4fa8-af23-fd296aa5abd5','docD','Av São João',-23.5513336,-46.6506514);
 
 
 
